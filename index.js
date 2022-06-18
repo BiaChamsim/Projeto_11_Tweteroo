b148359cac3b5a9e7a1426f5427a8d5993292528
Bia Chamsim <biachamsim@gmail.com>
2022-06-17 22:00:06 -0300

    avatar e usuario ajustados no get

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const usuarios = [];
 const tweets = [];
@@ -9,12 +11,19 @@ const tweets = [];
 app.post('/sign-up', (req, res) => {
     const usuario = req.body;
     usuarios.push(usuario);  
+    console.log(usuarios)
     res.send("Ok")  
 });
 
 app.post('/tweets', (req, res) => {
-    const tweet = req.body;
-    tweets.push(tweet);
+    const { username, tweet } = req.body;
+    const { avatar } = usuarios.filter((usuario) => usuario.username === username)[0]
+    tweets.push({
+        username,
+        avatar,
+        tweet
+    });
+    console.log(tweets)
     res.send("Ok")
 });
 
@@ -25,4 +34,4 @@ app.get('/tweets', (req, res) => {
 
 
 
-app.listen(5001);
+app.listen(5000);
